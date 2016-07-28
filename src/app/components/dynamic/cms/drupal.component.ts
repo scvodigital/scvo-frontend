d984bf326b719081e1978fd45502fadcbe507bde
Calum MacUisdean <calum@martinmacuisdean.eu>
2016-07-28 16:55:03 +0100

    Menu improvements and routing bug fix

--- src/app/components/dynamic/cms/drupal.component.ts
+++ src/app/components/dynamic/cms/drupal.component.ts
@@ -15,7 +15,7 @@ import { MarkdownPipe } from '../../../pipes/markdown.pipe';
     providers: [DrupalService],
     pipes: [MarkdownPipe]
 })
-export class DrupalComponent implements OnInit {
+export class DrupalComponent {
     content_status: Observable<any>;
     content_nid: Observable<any>;
     content_type: Observable<any>;
@@ -25,28 +25,31 @@ export class DrupalComponent implements OnInit {
     content_body: Observable<any>;
     content_body_format: Observable<any>;
     content: Observable<any>;
-    content_error: Boolean = false;
+    error: Boolean = false;
+    error_message: Observable<any>;
 
-    constructor(private router: Router, private _drupalService: DrupalService) {}
-
-    ngOnInit() {
-        console.log("Asking Drupal for "+this.router.url);
-        this._drupalService.loadPage(this.router.url)
-            .subscribe(
-                result => {
-                    this.content_status =                   (result.status[0]) ?                result.status[0].value : 0;
-                    if (this.content_status) {
-                        this.content_nid =                  (result.nid[0]) ?                   result.nid[0].value : '';
-                        this.content_type =                 (result.type[0]) ?                  result.type[0].target_id : 'page';
-                        this.content_revision_timestamp =   (result.revision_timestamp[0]) ?    result.revision_timestamp[0].value : '';
-                        this.content_title =                (result.title[0]) ?                 result.title[0].value : '';
-                        this.content_subheading =           (result.field_subheading[0]) ?      result.field_subheading[0].value : '';
-                        this.content_body =                 (result.body[0]) ?                  result.body[0].value : '';
-                        this.content_body_format =          (result.body[0]) ?                  result.body[0].format : '';
-                        this.content =                      (result) ?                          result : {};
-                    }
-                },
-                err => { this.content_error = true }
-            );
+    constructor(private router: Router, private _drupalService: DrupalService) {
+        this.router
+        .events
+        .subscribe(params => {
+            console.log("Asking Drupal for "+this.router.url);
+            this._drupalService.loadPage(this.router.url)
+                .subscribe(
+                    result => {
+                        this.content_status =                   (result.status[0]) ?                result.status[0].value : 0;
+                        if (this.content_status) {
+                            this.content_nid =                  (result.nid[0]) ?                   result.nid[0].value : '';
+                            this.content_type =                 (result.type[0]) ?                  result.type[0].target_id : 'page';
+                            this.content_revision_timestamp =   (result.revision_timestamp[0]) ?    result.revision_timestamp[0].value : '';
+                            this.content_title =                (result.title[0]) ?                 result.title[0].value : '';
+                            this.content_subheading =           (result.field_subheading[0]) ?      result.field_subheading[0].value : '';
+                            this.content_body =                 (result.body[0]) ?                  result.body[0].value : '';
+                            this.content_body_format =          (result.body[0]) ?                  result.body[0].format : '';
+                            this.content =                      (result) ?                          result : {};
+                        }
+                    },
+                    err => { this.error = true; this.error_message = err }
+                );
+            });
     }
 }
