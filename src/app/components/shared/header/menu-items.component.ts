21fb4b0e00ae9a5c845592ac7e5f92d1341c9f01
Calum MacUisdean <calum@martinmacuisdean.eu>
2016-07-28 15:55:42 +0100

    Dynamic menus, now working properly for mobile

--- src/app/components/shared/header/menu-items.component.ts
+++ src/app/components/shared/header/menu-items.component.ts
@@ -11,7 +11,177 @@ import { MaterializeDirective } from "angular2-materialize";
     directives: [ROUTER_DIRECTIVES, MaterializeDirective],
 })
 export class MenuItemsComponent {
-    @Input('menu-items') menuName: string = '';
+    @Input('menu-items') menuType: string = '';
+    public navigationMenu: Object;
 
-    constructor() {}
+    constructor() {
+        this.navigationMenu = [
+            {
+                "title": "Running your organisation",
+                "link": "/running-your-organisation",
+                "contents": [
+                    {
+                        "title": "Running your organisation",
+                        "link": "/running-your-organisation"
+                    },
+                    {
+                        "title": "Finance",
+                        "link": "/running-your-organisation/finance"
+                    },
+                    {
+                        "title": "Business management",
+                        "link": "/running-your-organisation/business-management"
+                    },
+                    {
+                        "title": "Governance",
+                        "link": "/running-your-organisation/governance"
+                    },
+                    {
+                        "title": "Funding",
+                        "link": "/running-your-organisation/funding"
+                    },
+                    {
+                        "title": "Legislation & regulation",
+                        "link": "/running-your-organisation/legislation-and-regulation"
+                    },
+                ]
+            },
+            {
+                "title": "Employability",
+                "link": "/employability",
+                "contents": [
+                    {
+                        "title": "Employability",
+                        "link": "/employability"
+                    },
+                    {
+                        "title": "Community Jobs Scotland",
+                        "link": "/employability/cjs"
+                    },
+                    {
+                        "title": "Disability equality internships",
+                        "link": "/employability/internships"
+                    },
+                    {
+                        "title": "Past employability schemes",
+                        "link": "/employability/past-schemes"
+                    }
+                ]
+            },
+            {
+                "title": "Products & services",
+                "link": "/products-and-services",
+                "contents": [
+                    {
+                        "title": "Products & services",
+                        "link": "/products-and-services"
+                    },
+                    {
+                        "title": "SCVO membership",
+                        "link": "/products-and-services/membership"
+                    },
+                    {
+                        "title": "Good HQ",
+                        "link": "/products-and-services/good-hq"
+                    },
+                    {
+                        "title": "Office space",
+                        "link": "/products-and-services/office-space"
+                    },
+                    {
+                        "title": "Credit Union",
+                        "link": "/products-and-services/credit-union"
+                    },
+                    {
+                        "title": "Third Force News",
+                        "link": "/products-and-services/tfn"
+                    },
+                    {
+                        "title": "Goodmoves",
+                        "link": "/products-and-services/goodmoves"
+                    },
+                    {
+                        "title": "Funding Scotland",
+                        "link": "/products-and-services/funding-scotland"
+                    },
+                    {
+                        "title": "Payroll",
+                        "link": "/products-and-services/payroll"
+                    },
+                    {
+                        "title": "Digital participation",
+                        "link": "/products-and-services/digital-scotland"
+                    },
+                    {
+                        "title": "Scottish Accessible Information Forum",
+                        "link": "/products-and-services/saif"
+                    },
+                    {
+                        "title": "Professional networks",
+                        "link": "/products-and-services/professional-networks"
+                    },
+                    {
+                        "title": "Affiliate deals",
+                        "link": "/products-and-services/affiliate-deals"
+                    }
+                ]
+            },
+            {
+                "title": "Events & training",
+                "link": "/events-and-training",
+                "contents": [
+                    {
+                        "title": "Events & training",
+                        "link": "/events-and-training"
+                    },
+                    {
+                        "title": "Scottish Charity Awards",
+                        "link": "/events-and-training/scottish-charity-awards"
+                    },
+                    {
+                        "title": "The Gathering",
+                        "link": "/events-and-training/the-gathering"
+                    },
+                    {
+                        "title": "Training courses",
+                        "link": "/events-and-training/training-courses"
+                    }
+                ]
+            },
+            {
+                "title": "Policy hub",
+                "link": "/policy-hub",
+                "contents": [
+                    {
+                        "title": "Policy hub",
+                        "link": "/policy-hub"
+                    },
+                    {
+                        "title": "Research",
+                        "link": "/policy-hub/research"
+                    },
+                    {
+                        "title": "Briefings & reports",
+                        "link": "/policy-hub/briefings-and-reports"
+                    },
+                    {
+                        "title": "Consultation responses",
+                        "link": "/policy-hub/consultation-responses"
+                    },
+                    {
+                        "title": "Blogs",
+                        "link": "/policy-hub/blogs"
+                    },
+                    {
+                        "title": "Campaigns",
+                        "link": "/policy-hub/campaigns"
+                    },
+                    {
+                        "title": "Policy committee",
+                        "link": "/policy-hub/policy-committee"
+                    }
+                ]
+            }
+        ]
+    }
 }
