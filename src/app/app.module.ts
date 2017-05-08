01b663d9d5292cb56161fec1df6cd9ed2abcf1ec
Calum MacUisdean <calum@martinmacuisdean.eu>
2017-05-08 13:52:43 +0100

    Fixed Disqus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import { DynamicComponentModule } from 'angular2-dynamic-component';
 import { SlimLoadingBarService, SlimLoadingBarModule } from 'ng2-slim-loading-bar';
 import { Ng2BreadcrumbModule } from 'ng2-breadcrumb/ng2-breadcrumb';
 import { NguiAutoCompleteModule } from '@ngui/auto-complete';
-import { DisqusModule } from 'ng2-awesome-disqus';
+import { DisqusModule } from 'ngx-disqus';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@angular/material';
