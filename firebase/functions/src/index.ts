4daa48fd5c15a8cf4c8a24a6f2ffddeb772fc55b
tonicblue <tonicblue@scvo-matthew.localdomain>
2017-06-23 08:05:08 +0100

    Added static route loading

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -43,6 +43,23 @@ exports.updateStaticContentIndex = functions.database.ref('/static-content/{id}/
 
 exports.loadRoute = functions.https.onRequest((req: functions.Request, res: functions.Response) => {
     var addressParts = Route.getRouteParts(req.get('referer'));
-    console.log(addressParts);
-    res.json(addressParts);
+    console.log('Address parts', addressParts);
+    var elasticUrl = 'https://readonly:onlyread@50896fdf5c15388f8976945e5582a856.eu-west-1.aws.found.io/web-content/';
+    if(!addressParts.typeId && !addressParts.querystring){
+        // Static content request
+        var docUrl = elasticUrl + 'static-content/' + addressParts.path.replace(/\//gi, '_');
+        console.log('Document Url', docUrl)
+        request.get(docUrl, (err, resp, body) => {
+            if(err){
+                console.error('Error fetching static content', addressParts, err);
+                res.json(err);
+            }else{
+                var obj = JSON.parse(body);
+                console.log('Document', obj);
+                res.json(obj);
+            }
+        });
+    }else{
+
+    }
 });
