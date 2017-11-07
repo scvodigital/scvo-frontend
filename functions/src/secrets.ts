bf5d45838e019b77c2aa25179f9a6acc3be4cec4
Matthew Wilkes <tonicblue@gmail.com>
2017-11-07 13:20:24 +0000

    Removing secrets

--- functions/src/secrets.ts
+++ functions/src/secrets.ts
@@ -1,13 +1,2 @@
 export const Secrets = {
-    "elasticsauceToken": "c2FsZXNmb3JjZTpKVHVTaXlUYlRCXl4qM0ghaCY3OGhsVldlb04hZEwydw==",
-      "type": "service_account",
-  "project_id": "scvo-net",
-  "private_key_id": "4fd2c1ef9b13926564dba03340eaa69b24121fdb",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCq6DnNED/CVx2h\nAlovleKrDZrlH33Y38UOerPaWLEcvkAqcGTeAkL4esdseAkbHH8XsyHtAfC1BIib\nUkdsZrlyfvOef3W+6DgA4dkYeO4Eao8qsBqaU/hysCXu3JzWlNrxPFuCDj+7vWaf\nSSZpht78HiLouEvpzZmXCRmUm6H22uDYV65IeDD97A4KVo5nhuwAkIsJhGRbnA8A\nkxDWgkugUQARkIr5u67y4++/U6petVi1i9koiYVn0CCT2t9qUxVD5qX0KDhH+MD7\nvzvMcORAyJmY+ohZ56n2JN+YaSZsxPcfPLuU3eT0UloinuwwuQFnqWiYjTyUdd5B\n/1Xnx8dZAgMBAAECggEAOslHPHx22fOaKuxCtjSBs6Gp4l7rLEKIJxtYquWwAjmE\nm7RuVZkEjTC6UNqXEz7BjpbcuAffUnx9OqPaEnThG/uD88RW7XQ180O8J3lNMbKv\njRjnBaeVgWmKmrdCsWKhTrIarckV7g9PccG5Fzo/kAwTqOXfosvIJVYJeP7xfpWo\n6oVqIVgsj4otXOmu1mLyvU+2Ku01ER0jgbZqxL4Pi++IPVVyA9OHDfzbVJ8uxVLA\nGnIqvnh76x8z81eWisfK/HfBXtCmMDp6RQaLcf20nm9frt5wrBiliFn2ohNSdzi2\nFQppWOE1kt5XeW2f279dNvuANGc5X4QcGRt0U8J/6wKBgQDvHeOO6AcfkF1LiMzx\nzpQyh9a+r4b0jUWtT5gcICLITr6cxINbrG9GqS8K0/kCVuoehob5Nd1DpqNxsRmT\nGTmhaqXWgg+QwQsCmkwOXb4cdRsmgO2ELnD8quAV6hDxz+6Vq17feL6/Kp0aRdJy\nylazZLNEMmbniUnbYV5SmATSkwKBgQC2+W0l51cLz1DecQlJvavXsYlOlZVvgdDw\nDlGdwsglmcByeXyZBUU+LbEKclLshqwT5MHHiqheWrMIiauAPr5pWMVHLimb/wC/\nnlPeT/6sbN6MHWdphNcFxWxO2b5GlWK65dmkZ1zEf1So9lUSDw6XjRvX0wveaxxA\nx0CtjrkV4wKBgGZKnwKPeBykoYSirSn0eal0OwWwCUes99XAaw39IWQXa6uc1aTu\nC1xrG9jB/ZBmphkQ68TIqjM6okkMvigTbngDK5asZQRD0PX4E/lBWQbLgiXimbkm\nNOeiIeK+EymfDi2w9hEbkl4tWK/YlsLMlHKB7b6K4ULDlHjpGnSy271JAoGBAJ3z\n/kxNQ/CW/Gm6TNlSmkjuIukq6kDDYRoKUlyNLHiBHQf1ZiE20oqVqRKqlHlI3L2j\nqcDXKveg9pK1Pc7M21VX453J9POMlBnW1PHCPsichxWPwJjvqyt5PUFoaQDHrt5G\n07Y/4G5kuTHOzKt1QhSEJPgQYVuVPWOSXnw1DAQfAoGAJmVq//CQxlPnwEfHjsx7\nUP94ixeUOAPR6dKUESHVMwuTvDER6iRY1zmKO76O6rxoIQkYuSrZcEVyH0HPrxEt\n1Z8OsOt0f2lZieSdNi+i2nYZ/Wk/OGAwNCGNbRSXKsDJcKaE7455lwg+EzXGr7mV\nkLcXB3bcfN/wXUPgZeFslyM=\n-----END PRIVATE KEY-----\n",
-  "client_email": "scvo-net@appspot.gserviceaccount.com",
-  "client_id": "117500144193081474249",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://accounts.google.com/o/oauth2/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/scvo-net%40appspot.gserviceaccount.com"
 }
