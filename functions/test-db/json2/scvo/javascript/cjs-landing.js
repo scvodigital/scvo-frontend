c5d3bcdbf67c9f53b308ff84f0aa490b05f7ee88
Calum MacUisdean <calum@martinmacuisdean.eu>
2018-01-25 14:50:59 +0000

    Gathering work

--- functions/test-db/json2/scvo/javascript/cjs-landing.js
+++ functions/test-db/json2/scvo/javascript/cjs-landing.js
@@ -1,13 +1,16 @@
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-});
+var swiper = new Swiper(
+    '.swiper-container',
+    {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        }
+    }
+);
