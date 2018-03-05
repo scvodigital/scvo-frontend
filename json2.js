714e8960b7a035e962642504c962f6e5b011a69a
Matthew Wilkes <tonicblue@gmail.com>
2018-03-05 20:36:13 +0000

    Upgraded JSON2 to appear to be valid JSON

--- json2.js
+++ json2.js
@@ -10,13 +10,11 @@ if(process.argv.length < 2){
 }
 
 var input = process.argv.slice(2);
-var output = input.pop();;
+var output = input.pop();
     
 const re = {
-    blockComment: /(\/\*(?:.|[\r\n])*?\*\/)(?=([^"]*"[^"]*")*[^"]*$)/igm,
-    fancyStrings: /(\`(?:.|[\r\n])*?\`)/igm,
-    trailingComma: /(\,)(\s+)(\}|\])/igm,
-    fileImport: /(?:\{\:)(.*?)(?:\})/ig,
+    stringImport: /(?:"\{\$)(.*?)(?:\}")/ig,
+    jsonImport: /(?:"\{\:)(.*?)(@.+?)?(?:\}")/ig,
 };
 const importCache = {};
 
@@ -53,58 +51,40 @@ function processFile(path){
 
     var basedir = pa.dirname(path);
 
-    //import external content
-    var fileImports = after.match(re.fileImport) || [];
-    console.log(chalk.blueBright(chalk.bold('\tImports:'), fileImports.length));
-    var after = after.replace(re.fileImport, (all, match) => {
-        //split apart our import path and potention object path
-        var parts = match.split('|');
-        var importPath = parts[0];
-
-        var fullImportPath = pa.join(basedir, importPath);
+    //import external strings
+    var stringImports = after.match(re.stringImport) || [];
+    console.log(chalk.blueBright(chalk.bold('\tString Imports:'), stringImports.length));
+    var after = after.replace(re.stringImport, (all, match) => {
+        var fullImportPath = pa.join(basedir, match);
         //check to see if we have already read this file
         if(!importCache.hasOwnProperty(fullImportPath)){
             //if not, load it into our import cache
             importCache[fullImportPath] = fs.readFileSync(fullImportPath).toString();
         }
         
-        //check if we have a second parameter (object path)
-        if(parts.length > 1){
-            //if we do then we treat the object as json
-            try{
-                var objectPath = parts[1];
-                var json = JSON.parse(importCache[fullImportPath]);
+        var jsonSafe = JSON.stringify(importCache[fullImportPath]);
 
-                //and extract the value from the given object path
-                var value = dot.pick(objectPath, json);
-                return JSON.stringify(value, null, 4);
-            }catch(err){
-                console.error(chalk.red(chalk.bold('\tError extracting value from json:'), err.message));
-                return '';
-            }
-        }else{
-            return importCache[fullImportPath];
-        }
+        return jsonSafe;
     });
     
-    //remove block comments
-    //var blockComments = after.match(re.blockComment) || [];
-    //console.log(chalk.blueBright(chalk.bold('\tBlock Comments:'), blockComments.length));
-    //var after = after.replace(re.blockComment, '');
+    var jsonImports = after.match(re.jsonImport) || [];
+    console.log(chalk.blueBright(chalk.bold('\tJSON Imports:'), jsonImports.length));
+    var after = after.replace(re.jsonImport, (all, match, match2) => {
+        var fullImportPath = pa.join(basedir, match);
+        //check to see if we have already read this file
+        if(!importCache.hasOwnProperty(fullImportPath)){
+            //if not, load it into our import cache
+            importCache[fullImportPath] = fs.readFileSync(fullImportPath).toString();
+        }
+        
+        var obj = JSON.parse(importCache[fullImportPath]);
 
-    //remove trailing commas
-    var trailingCommas = after.match(re.trailingComma) || [];
-    console.log(chalk.blueBright(chalk.bold('\tTrailing Commas:'), trailingCommas.length));
-    var after = after.replace(re.trailingComma, '$2$3');
+        if (match2) {
+            match2 = match2.substr(1);
+            obj = dot.pick(match2, obj);
+        }
 
-    //fix fancy strings
-    var fancyStrings = after.match(re.fancyStrings) || [];
-    console.log(chalk.blueBright(chalk.bold('\tFancyStrings:'), fancyStrings.length));
-    after = after.replace(re.fancyStrings, (match) => {
-        match = match.replace(/\`/ig, '');
-        match = JSON.stringify(match);
-
-        return match;
+        return JSON.stringify(obj);
     });
     
     var fileName = pa.parse(path).base;
@@ -123,8 +103,11 @@ function processFile(path){
         var positionMatch = err.message.match(/(?:position\s)(\d+)/);
         var position = positionMatch ? positionMatch[1] : null;
         var lines = after.split('\n');
-        var debug = '';
+        var debug = [];
         var totalChars = 0;
+        var lineNo = 0;
+        var colNo = 0;
+
         lines.forEach((line, number) => {
             var lineNoString = parseInt(number + 1) + ': ';
             if(position){
@@ -132,16 +115,24 @@ function processFile(path){
                 if(position >= totalChars && position < totalChars + line.length){
                     var linePosition = position - totalChars;
                     var offset = lineNoString.length;
+                    colNo = offset;
                     var spaces = Array(linePosition + offset).join(' ');
-                    debug += spaces + '^ ' + err.message + '\n';
+                    debug.push(spaces + '^ ' + err.message);
+                    lineNo = number;
                 }
             }
-            debug += lineNoString + line + '\n';
+            
+            debug.push(lineNoString + line);
             totalChars += line.length;
         });
+
+        debug = debug.splice(lineNo - 5, 10);
         console.error(chalk.bgRed.black.bold('There was a problem with the final JSON'));
         console.error(chalk.red.bold(err));
-        console.error(chalk.gray(debug));
+        console.error(chalk.gray(debug.join('\n')));
+        console.error(chalk.red.bold('File:', newPath + '.broken'));
+        console.error(chalk.red.bold('Line No:', lineNo));
+        console.error(chalk.red.bold('Column No:', colNo));
         console.error(chalk.red.bold(err));
 
         fs.writeFileSync(newPath + '.broken', after);
