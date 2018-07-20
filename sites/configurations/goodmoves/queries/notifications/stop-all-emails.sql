aaf59c8b11e2d251394252ddff1afeb215f77a14
Matthew Wilkes <tonicblue@gmail.com>
2018-07-20 14:00:25 +0100

    Created Stop All Emails route and fixed update SQL

--- sites/configurations/goodmoves/queries/notifications/stop-all-emails.sql
+++ sites/configurations/goodmoves/queries/notifications/stop-all-emails.sql
@@ -1,3 +1,6 @@
 UPDATE subscriptions
 SET subscribed = 0
-WHERE campaign IN ('{{@root.context.metaData.emailCampaignName}}', '{{@root.context.metaData.shortlistCampaignName}}');
+WHERE 
+  campaign IN ('{{@root.context.metaData.emailCampaignName}}', '{{@root.context.metaData.shortlistCampaignName}}') AND
+  email = '{{@root.data.auth.email}}';
+
