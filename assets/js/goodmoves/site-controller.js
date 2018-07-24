5d18f0055d56aeb86436b41b491baa0b7fc01569
Matthew Wilkes <tonicblue@gmail.com>
2018-07-24 15:46:22 +0100

    Occasional draw mostly styled!

--- assets/js/goodmoves/site-controller.js
+++ assets/js/goodmoves/site-controller.js
@@ -45,6 +45,15 @@ var GoodmovesController = Class.extend({
 
   displayModeChanged: function() {
     // console.log('Display Mode:', this.displayMode);
+    if (this.displayMode === 'desktop') {
+      //$('#sidebar-temporary')
+      //  .removeClass('mdc-drawer--temporary')
+      //  .addClass('mdc-drawer--permanent');
+    } else {
+      //$('#sidebar-temporary')
+      //  .removeClass('mdc-drawer--permanent')
+      //  .addClass('mdc-drawer--temporary');
+    }
   },
 
   setupMaterialDesignComponents: function() {
@@ -243,11 +252,10 @@ function handleMaps() {
   var maps = $('[data-map-options]').each(function(i, o) {
     var options = $(o).data('map-options');
     var map = L.map(o).setView([51.505, -0.09], 13);
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic2N2b2RpZ2l0YWwiLCJhIjoiY2pqemVqbTA4MDNibTNrbzV2OHpkb3BkcCJ9.Tz193u3OIAAMDT9GJOV50g', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Imagery &amp; Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
         maxZoom: 18,
-        id: 'mapbox.streets',
-        accessToken: 'pk.eyJ1Ijoic2N2b2RpZ2l0YWwiLCJhIjoiY2pqemVqbTA4MDNibTNrbzV2OHpkb3BkcCJ9.Tz193u3OIAAMDT9GJOV50g'
+        id: 'mapbox.streets'
     }).addTo(map);
 
     var mapName = $(o).data('map-name');
