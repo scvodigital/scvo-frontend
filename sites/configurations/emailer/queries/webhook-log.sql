91b5616ab662952b42fda3e7559b85376d84f5f6
Matthew Wilkes <tonicblue@gmail.com>
2018-08-24 09:33:41 +0100

    Fixing webhook log sql template

--- sites/configurations/emailer/queries/webhook-log.sql
+++ sites/configurations/emailer/queries/webhook-log.sql
@@ -1,18 +1,19 @@
 /*
   {{{stringify @root.request.body}}}
 */
+{{#if @root.request.body.event-data}}
 CALL _log(
   {{~#with @root.request.body.event-data}}
-    {{{stringify id}}},
-    {{{stringify message.headers.message-id}}},
-    {{{stringify tags.[0]}}},
-    {{{stringify recipient}}},
+    {{{stringify id}}}, -- uid
+    {{{stringify message.headers.message-id}}}, -- messageId
+    {{{stringify tags.[0]}}}, -- campaign
+    {{{stringify recipient}}}, -- email
     {{#compare event "failed"~}}
-      {{{stringify (concat "failed-" severity)}}},
+      {{{stringify (concat "failed-" severity)}}}
     {{else}}
-      {{{stringify event}}},
-    {{~/compare}}
-    {{{stringify log-level}}},
+      {{{stringify event}}}
+    {{~/compare}}, -- event
+    {{{stringify log-level}}}, -- logLevel
     {{#compare event "delivered"~}}
       {{{stringify storage.url}}}
     {{else}}
@@ -21,13 +22,18 @@ CALL _log(
       {{else}}
         {{~#compare event "clicked"~}}
           {{{stringify url}}}
+        {{else}}
+          ''
         {{~/compare~}}
       {{~/compare~}}
-    {{~/compare}}
+    {{~/compare}}, -- other
   {{~/with}}
   {{~#with @root.request.body.signature~}}
-    {{{stringify timestamp}}},
-    {{{stringify token}}},
-    {{{stringify signature}}} 
+    {{{stringify timestamp}}}, -- timestamp
+    {{{stringify token}}}, -- token
+    {{{stringify signature}}} -- signature
   {{~/with}}
 );
+{{else}}
+SET @query=false;
+{{/if}}
